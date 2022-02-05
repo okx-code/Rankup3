e50f04f2ceac833d324663754fbab0cbe5d7280c
NicBOMB <19939315+NicBOMB@users.noreply.github.com>
2022-02-05 13:02:50 -0800

    add null checks to prevent log spam when tree is null (failed to build) If there's a possibility that the tree is going to be null we should account for it rather than let the plugin spam like: Caused by: java.lang.NullPointerException: Cannot invoke "sh.okx.rankup.ranks.RankTree.asList()" because "this.tree" is null         at sh.okx.rankup.ranks.RankList.getByPlayer(RankList.java:110) ~[Rankup-3.13.2.jar:?]

--- src/main/java/sh/okx/rankup/ranks/RankList.java
+++ src/main/java/sh/okx/rankup/ranks/RankList.java
@@ -81,9 +81,7 @@ public abstract class RankList<T extends Rank> {
   }
 
   public T getRankByName(String name) {
-    if (name == null) {
-      return null;
-    }
+    if (name == null || tree == null){return null;}
     for (T rank : tree) {
       if (name.equalsIgnoreCase(rank.getRank())) {
         return rank;
@@ -93,9 +91,7 @@ public abstract class RankList<T extends Rank> {
   }
 
   public RankElement<T> getByName(String name) {
-    if (name == null) {
-      return null;
-    }
+    if (name == null || tree == null){return null;}
     List<RankElement<T>> rankElements = tree.asList();
     for (RankElement<T> rank : rankElements) {
       if (name.equalsIgnoreCase(rank.getRank().getRank())) {
@@ -107,6 +103,7 @@ public abstract class RankList<T extends Rank> {
 
 
   public RankElement<T> getByPlayer(Player player) {
+    if (tree == null){return null;}
     List<RankElement<T>> list = tree.asList();
     Collections.reverse(list);
     for (RankElement<T> t : list) {
@@ -118,6 +115,7 @@ public abstract class RankList<T extends Rank> {
   }
 
   public T getRankByPlayer(Player player) {
+    if (tree == null){return null;}
     List<RankElement<T>> list = tree.asList();
     Collections.reverse(list);
     for (RankElement<T> t : list) {
